73172ad17f2db44ece591953541a9feb44e6f4d3
chuoer47 <lingchen47@outlook.com>
2026-09-24 09:59:06 +0800

    feat: 侧边栏面板支持一键收起/展开（桌面端悬浮切换按钮，状态存 localStorage）

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,13 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
+import SidebarToggle from "./SidebarToggle.vue";
 import "./custom.css";
-export default DefaultTheme;
+
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "layout-top": () => h(SidebarToggle),
+    });
+  },
+};
