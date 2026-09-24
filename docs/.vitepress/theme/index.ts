87774fa590091582902898f336c6025f1fcdcf40
chuoer47 <lingchen47@outlook.com>
2026-09-24 10:09:36 +0800

    Revert "feat: 侧边栏面板支持一键收起/展开（桌面端悬浮切换按钮，状态存 localStorage）"
    This reverts commit 73172ad17f2db44ece591953541a9feb44e6f4d3.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,3 @@
 import DefaultTheme from "vitepress/theme";
-import { h } from "vue";
-import SidebarToggle from "./SidebarToggle.vue";
 import "./custom.css";
-
-export default {
-  extends: DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      "layout-top": () => h(SidebarToggle),
-    });
-  },
-};
+export default DefaultTheme;
